70898704fd3efac00739284bd34dd27122bbaf3b
Sam-web-hub <sultanan420@gmail.com>
2021-05-19 19:03:47 +0600

    Fixed the CSS properties

--- src/App.js
+++ src/App.js
@@ -21,14 +21,14 @@ const reset = () => {
 
 
   return (
-    <div className="App dib mw5 mw7-ns center pa3 ph5-ns vh-100 dt w-100">
+    <div className="App dib center pa7 ph5-ns vh-100 dt w-100">
       <div className="f1 v-mid tc white ph4-l">
         <h1>{count}</h1>
       </div>
-      <div className="pointer pa3 ma2 f6 f2-m f-subheadline-l fw6 tc">
-        <button className="br2 f1 grow" onClick={decrease}>Decrease</button>
-        <button className="br2 f1 grow" onClick={increase}>Increase</button>
-        <button className="br2 f1 grow" onClick={reset}>Reset</button>
+      <div className="pointer pa3 tc">
+        <button className="br2 f1 grow shadow-5" onClick={decrease}>Decrease</button>
+        <button className="br2 f1 grow shadow-5" onClick={increase}>Increase</button>
+        <button className="br2 f1 grow shadow-5" onClick={reset}>Reset</button>
       </div>
     </div>
   );
